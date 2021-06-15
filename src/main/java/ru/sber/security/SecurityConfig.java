18c65ce0ee4335b08da69c8339cb3d292b1087af
Roman Ivanov <roivanoff@gmail.com>
2021-06-15 13:51:49 +0300

    add role

--- src/main/java/ru/sber/security/SecurityConfig.java
+++ src/main/java/ru/sber/security/SecurityConfig.java
@@ -2,6 +2,8 @@ package ru.sber.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.User;
@@ -10,10 +12,23 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import ru.sber.security.customer.Role;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .authorizeRequests()
+                .antMatchers("/").permitAll()
+                .antMatchers(HttpMethod.GET, "/api/v1/**").hasAnyRole(Role.ADMIN.name())
+                .anyRequest()
+                .authenticated()
+                .and()
+                .httpBasic();
+    }
+
     @Bean
     @Override
     protected UserDetailsService userDetailsService() {
@@ -22,13 +37,13 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .roles("ADMIN")
                 .build();
         UserDetails customer = User.builder().username("customer")
-                .password("{noop}customer")
+                .password(passwordEncoder().encode("customer"))
                 .roles("CUSTOMER")
                 .build();
         return new InMemoryUserDetailsManager(admin, customer);
     }
 
-    //@Bean
+    @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
     }
---
+++ src/main/java/ru/sber/security/customer/Role.java
@@ -0,0 +1,6 @@
+package ru.sber.security.customer;
+
+public enum Role {
+    ADMIN,
+    CUSTOMER;
+}
